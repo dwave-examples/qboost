29580707edf3dbcb9db826d60f6e806ee7290a8b
John McFarland <jmcfarland@dwavesys.com>
2021-02-12 13:14:55 -0800

    Update demo with option to analyze digits data set

--- demo.py
+++ demo.py
@@ -17,7 +17,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 from qboost import qboost_lambda_sweep
-from datasets import make_blob_data
+from datasets import make_blob_data, get_handwritten_digits_data
 
 
 if __name__ == '__main__':
@@ -27,11 +27,15 @@ if __name__ == '__main__':
     parser.add_argument('--verbose', action='store_true')
 
     subparsers = parser.add_subparsers(title='dataset', description='dataset to use', dest='dataset', required=True)
+
     sp_blobs = subparsers.add_parser('blobs', help='blobs data set')
-    
     sp_blobs.add_argument('--num-samples', type=int, default=2000, help='number of samples (default: %(default)s)')
     sp_blobs.add_argument('--num-features', type=int, default=10, help='number of features (default: %(default)s)')
     sp_blobs.add_argument('--num-informative', type=int, default=2, help='number of informative features (default: %(default)s)')
+
+    sp_digits = subparsers.add_parser('digits', help='handwritten digits data set')
+    sp_digits.add_argument('--digit1', type=int, default=0, choices=range(10), help='first digit to include (default: %(default)s)')
+    sp_digits.add_argument('--digit2', type=int, default=1, choices=range(10), help='second digit to include (default: %(default)s)')
 
     args = parser.parse_args()
 
@@ -53,5 +57,27 @@ if __name__ == '__main__':
         print('best lambda:', lam)
         print('informative features:', list(range(n_informative)))
         print('   selected features:', qboost.get_selected_features())
+
+        print('score on test set:', qboost.score(X_test, y_test))
+
+
+    if args.dataset == 'digits':
+        if args.digit1 == args.digit2:
+            raise ValueError("must use two different digits")
+
+        X, y = get_handwritten_digits_data(args.digit1, args.digit2)
+        n_features = np.size(X, 1)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+        print('Number of features:', np.size(X, 1))
+        print('Number of training samples:', len(X_train))
+        print('Number of test samples:', len(X_test))
+
+        normalized_lambdas = np.linspace(0.0, 1.75, 10)
+        lambdas = normalized_lambdas / n_features
+        qboost, lam = qboost_lambda_sweep(X_train, y_train, lambdas, verbose=args.verbose)
+
+        print('best lambda:', lam)
+        print('number of selected features:', len(qboost.get_selected_features()))
 
         print('score on test set:', qboost.score(X_test, y_test))
